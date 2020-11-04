9860c9db9ea0abaf9d90ae91074b5e53a6063edb
RasmusHRAAU <34272322+RasmusHRAAU@users.noreply.github.com>
2020-11-04 09:41:45 +0100

    Git updater (#2)
    * Create git_updater.py
    
    * Create requirements.txt
    
    * Update git_updater.py

---
+++ git_updater.py
@@ -0,0 +1,36 @@
+from git import Repo
+import subprocess
+import shutil
+
+RASPHOME = '/home/pi/'
+GIT_DIRECTORY = RASPHOME + 'raspmesh'
+GIT_URL = 'https://github.com/Winerp7/Rasp-mesh.git'
+
+if __name__ == '__main__':
+    
+    # Ping google to check internet connection
+    ping_command = ['ping', '-c', '1', 'google.com']
+    has_internet = subprocess.call(ping_command) == 0
+
+    if has_internet:
+        # Delete the current directory
+        try:
+            shutil.rmtree(GIT_DIRECTORY)
+            print('Directory deleted')
+        except:
+            print('Failed to delete the directory')
+
+        # Download the newest repository
+        try:
+            Repo.clone_from(GIT_URL, GIT_DIRECTORY)
+            print('Repository cloned')
+        except:
+            print('Failed to clone the repository')
+
+        # Pip install all requirements    
+        try:
+            requirements_path = GIT_DIRECTORY + '/' + 'requirements.txt'
+            subprocess.call(['pip3', 'install', '-r', requirements_path])
+            print('requirements installed')
+        except:
+            print('Failed to install pip requirements')
