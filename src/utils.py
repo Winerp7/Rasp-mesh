cbf9aec539094eb5d3c9e6ce6b34ea615a5e16a4
RasmusHRAAU <34272322+RasmusHRAAU@users.noreply.github.com>
2020-11-04 14:40:18 +0100

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,18 @@
 from time import sleep
 
-
 def delay(ms):
     sleep(ms/1000.0)
+    
+def getserial():
+  # Extract serial from cpuinfo file
+  cpuserial = "0000000000000000"
+  try:
+    f = open('/proc/cpuinfo','r')
+    for line in f:
+      if line[0:6]=='Serial':
+        cpuserial = line[10:26]
+    f.close()
+  except:
+    cpuserial = "ERROR000000000"
+ 
+  return cpuserial
