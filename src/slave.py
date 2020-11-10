2500150d9141ae3bce5e960618603eb968862465
Rasmus <rhra17@student.aau.dk>
2020-11-10 12:59:38 +0100

    test

--- src/slave.py
+++ src/slave.py
@@ -1,19 +1,29 @@
 from mesh import MeshNet
 from utils import delay
 from struct import *
+from time import time
+
+start = time()
+
+def millis():
+    return int((time()-start)*1000) % (2 ** 32)
 
 
 def main():
     mesh = MeshNet(master=False)
 
     message = 'Hello this string is not too big'
+    displayTimer = millis()
     
     while True: 
         mesh.update()
 
-        write_successful = mesh.send(message)
-        if write_successful:
-            print(f'Sent: {message}', flush=True)
+        if (millis() - displayTimer) >= 1000:
+            displayTimer = millis()
+
+            write_successful = mesh.send(message)
+            if write_successful:
+                print(f'Sent: {message}', flush=True)
 
         delay(1)
         
