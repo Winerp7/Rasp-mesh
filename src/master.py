14db4348650677f271c42bc9c63088b4fa066886
Rasmus <rhra17@student.aau.dk>
2020-11-10 12:47:49 +0100

    fixes

--- src/master.py
+++ src/master.py
@@ -12,10 +12,14 @@ def main():
         
         for from_node, message in mesh.read():
             print(message, flush=True)
-            r = requests.post('http://192.168.43.105:3000/api-test', data = {'id': message})
-            print(r.status_code, flush=True)
-            if r.status_code == 200:
-                print(r.text, flush=True)
+
+            try:
+                r = requests.post('http://192.168.43.105:3000/api-test', data = {'id': message})
+                print(r.status_code, flush=True)
+                if r.status_code == 200:
+                    print(r.text, flush=True)
+            except:
+                print('Failed to contact server')
 
 
 
