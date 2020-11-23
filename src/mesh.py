b24d79f13bcaf820a48826822a60d1b616c6b843
Rasmus <rhra17@student.aau.dk>
2020-11-23 15:18:44 +0100

    added multi message

--- src/mesh.py
+++ src/mesh.py
@@ -17,12 +17,15 @@ CS_PIN = 0
 BUFFER_LENGTH = 100
 WRITE_INTERVAL = 100
 ECC_SYMBOLS = 14
+MAX_MESSAGE_SIZE = MAX_PAYLOAD_SIZE - ECC_SYMBOLS
+MULTI_MESSAGE_DELAY = 10
 
 class MeshNet:
     MSG_TYPE_INIT = 30              # ascii -> 0
     MSG_TYPE_UPDATE = 31            # ascii -> 1
     MSG_TYPE_UPDATE_CONFIRM = 32    # ascii -> 2
     MSG_TYPE_DATA = 33              # ascii -> 3
+    MSG_TYPE_MULTI = 34             # ascii -> 4
 
     def __init__(self, master=False):
         self.is_master = master
@@ -50,7 +53,6 @@ class MeshNet:
 
     def send_message(self, message_type, message, to_address=0):  # Addresses to master by default
         encoded = str.encode(message) # Convert string to byte array
-        encoded = self.error_corrector.encode(encoded) # Add Error Correcting SYMBOLS
         self.write_buffer.append((message_type, encoded, to_address))
 
     def add_message_callback(self, message_type, callback):
@@ -70,10 +72,16 @@ class MeshNet:
         while self.network.available():
             header, payload = self.network.read(MAX_PAYLOAD_SIZE)
 
-            if header.type in self.message_callbacks:
-                message = self.error_corrector.decode(payload)[0]  # Correct any bit flips
-                message = message.decode() # Convert from byte array to string
+            message = self.error_corrector.decode(payload)[0]  # Correct any bit flips
+            message = message.decode() # Convert from byte array to string
 
+            while header.type == MeshNet.MSG_TYPE_MULTI:
+                header, payload = self.network.read(MAX_PAYLOAD_SIZE)
+
+                ecc_message = self.error_corrector.decode(payload)[0]
+                message += ecc_message.decode()
+
+            if header.type in self.message_callbacks:
                 callback = self.message_callbacks[header.type]
                 callback(header.from_node, message)
 
@@ -82,13 +90,30 @@ class MeshNet:
             return
 
         message_type, message, to_address = self.write_buffer.pop()
-        
-        write_successful = self.mesh.write(to_address, message, message_type)
+
+        if len(message) > MAX_MESSAGE_SIZE:
+
+        else: 
+            message = self.error_corrector.encode(message) # Add Error Correcting SYMBOLS
+            write_successful = self.mesh.write(to_address, message, message_type)
 
         if not write_successful:
             self.write_buffer.append((message_type, message, to_address)) # if the message fails to send put it in the back of the queue
             if not self.is_master:
                 self._renewAddress()
+
+    def _multi_message_write(to_address, message, message_type):
+        chunks = [message[i:i+MAX_MESSAGE_SIZE] for i in range(0, len(message), MAX_MESSAGE_SIZE)] # split message in to chunks
+        
+        for chunk in chunks[:-1]: # loop through all chunks except for the last one
+            chunk = self.error_corrector.encode(chunk)
+            write_successful = self.mesh.write(to_address, chunk, MeshNet.MSG_TYPE_MULTI)
+            if not write_successful:
+                break
+
+            delay(MULTI_MESSAGE_DELAY)
+
+        self.mesh.write(to_address, chunks[-1], message_type)
 
     def _renewAddress(self):
         if not self.mesh.checkConnection():
