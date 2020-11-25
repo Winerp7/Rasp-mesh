a4f2b97a51face05ccb07a12088ca26b501ddf05
Rasmus <rhra17@student.aau.dk>
2020-11-25 13:52:19 +0100

    hee

--- src/api.py
+++ src/api.py
@@ -4,16 +4,16 @@ URL = 'http://192.168.43.105:3000/pi/'
 
 class Api:
     def __init__(self):
-        pass
+        self.id = '5f956e6dab148ff21ca3d084'
 
     def create_url(self, path):
-        return URL + path 
+        return URL + path
 
     def post_request(self, path, message):
         url = self.create_url(path)
         response = None
         try:
-            response = requests.post(url, json=message)
+            response = requests.post(url, json=message, params={'id': self.id})
         except Exception as e:
             print(e)
         return response
@@ -22,7 +22,7 @@ class Api:
         url = self.create_url(path)
         response = None
         try:
-            response = requests.get(url, json=message)
+            response = requests.get(url, json=message, params={'id': self.id})
         except Exception as e:
             print(e)
         return response
